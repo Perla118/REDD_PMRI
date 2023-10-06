79b7031baeb57f41989b1d5afd08fc68aa48b37f
Mapacheee03 <pablobusso8@gmail.com>
2023-10-06 10:57:24 -0700

    Agregar archivos de proyecto.

---
+++ Clases/calcular.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Examen_PMRI.Clases
+{
+    internal class calcular
+    {
+        //public void curp (string curppersona,char genero) 
+        //{
+        //    int longitud = 18;
+        //    curppersona = longitud;
+        //    curppersona = curppersona.ToUpper()
+        //}
+
+        public void intervalo(int num1, int num2)
+        {
+            if ()
+            {
+
+            }
+        }
+        public void multiplicar (int num1, int num2) 
+        { 
+            for (int i= 1; i < num2; i++)
+            {
+                Console.WriteLine($"La multiplicacion es: " + num2 * num1= num2 * num1);
+            }
+            
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using Examen_PMRI.Clases;
+
+namespace Examen_PMRI
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //calcular curp= new calcular();
+
+            //Console.WriteLine("Ingresa la CURP");
+            //string curppersona = Console.ReadLine();
+
+
+            //Console.WriteLine("Ingresa tu primer numero");
+            //int num1=Convert.ToInt32(Console.ReadLine());
+
+            //Console.WriteLine("Ingresa tu segundo numero");
+            //int num2 = Convert.ToInt32(Console.ReadLine());
+
+
+            //Console.WriteLine("La secuencia es: ");
+
+
+
+
+
+            calcular calcular = new calcular();
+
+            Console.WriteLine("Ingresa el numero a multiplicar");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+
+            //calcular.multiplicar(num1);
+        }
+    }
+}
